e602c6e2e3385425af6a51761ca5c1f9da363bcb
fish <zdd121048479@163.com>
2019-04-20 19:07:17 +0800

    parse a html file, and find its visible body.

--- 08_find_body.py
+++ 08_find_body.py
@@ -11,19 +11,19 @@ html_file = r'test_htmls\test_zhihu.html'
 
 
 def remove_tag(html):
+    """ remove script, style content and all tags"""
     get_all_body = re.search(r'<body.*?>.*?</body>', html, re.DOTALL)
     if get_all_body:
         body = get_all_body.group()
-        body = re.sub(r'<script.*?>.*?</script>', ' ', body, flags=re.DOTALL)
-        body = re.sub(r'<style.*?>.*?</style>', ' ', body, flags=re.DOTALL)
-        body_content = re.sub(r'".*?"', ' ', body)
-        body_content1 = re.sub(r'<[^<>]+?>', ' ', body_content)
-        body_content2 = re.sub(r'<[^<>]+?>', ' ', body_content1)
-        body_content3 = re.sub(r'\n\s*\n', '\n', body_content2, flags=re.MULTILINE)
-        return body_content3
     else:
-        return 'there is no body in this html file'
-
+        body = html
+    body = re.sub(r'<script.*?>.*?</script>', ' ', body, flags=re.DOTALL)
+    body = re.sub(r'<style.*?>.*?</style>', ' ', body, flags=re.DOTALL)
+    body_content = re.sub(r'".*?"', ' ', body)
+    body_content1 = re.sub(r'<[^<>]+?>', ' ', body_content)
+    body_content2 = re.sub(r'<[^<>]+?>', ' ', body_content1)
+    body_content3 = re.sub(r'\n\s*\n', '\n', body_content2, flags=re.MULTILINE)
+    return body_content3
 
 def parse_file(file):
     with open(file, encoding='utf-8') as f:
